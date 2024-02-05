7c9210a6036f41966a085b840302fa45ce687447
Elian Felix <efelix.v@gmail.com>
2024-02-05 18:55:52 -0500

    Add models to service microservice

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=300, primary_key=True)
+    sold = models.BooleanField(default=False)
+
+
+class Technician(models.Model):
+    employee_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
+
+class Appointment(models.Model):
+    vin = models.CharField(max_length=300)
+    status = models.PositiveSmallIntegerField(default=0)
+    date_time = models.DateTimeField()
+    reason = models.CharField(max_length=300)
+    customer = models.CharField(max_length=200)
+    technician = models.ForeignKey(Technician,
+                                   related_name='appointments',
+                                   on_delete=models.PROTECT)
