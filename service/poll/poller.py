05dbce159bddbb4ba580162f58f20006cadbc1c6
Elian Felix <efelix.v@gmail.com>
2024-02-05 17:47:45 -0500

    Add poller logic to service api

--- service/poll/poller.py
+++ service/poll/poller.py
@@ -10,8 +10,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "service_project.settings")
 django.setup()
 
 # Import models from service_rest, here. Ignore vs-code error hinting
-# from service_rest.models import Something
-
+from service_rest.models import AutomobileVO
+auto_url = 'http://inventory-api:8000/api/automobiles/'
 
 def poll():
     while True:
@@ -19,8 +19,12 @@ def poll():
         try:
             # Write your polling logic, here
             # Do not copy entire file
-            pass
-        
+            resp = requests.get(auto_url)
+            content = json.loads(resp.content)
+            [AutomobileVO.objects.update_or_create(
+                vin=aut['vin'],
+                sold=aut['sold'])
+             for aut in content['autos']]
         except Exception as e:
             print(e, file=sys.stderr)
 
