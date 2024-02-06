260d2e0c28cee9fd7a1e5163646aa54963637d4d
Shiela Lee <leeshiela@gmail.com>
2024-02-05 22:27:43 -0500

    Began front end with adding a salesperson

---
+++ ghi/app/src/AddSalesperson.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+
+function AddSalesperson() {
+    const [formData, setFormData] = useState({
+        first_name: "",
+        last_name: "",
+    })
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const url = "http://localhost:8090/api/salespeople/";
+
+        const fetchConfig = {
+            method: "POST",
+            body: JSON.stringify(formData),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+
+        const response = await fetch(url, fetchConfig);
+        if (response.ok) {
+            setFormData({
+                first_name: "",
+                last_name: "",
+            });
+        };
+    }
+
+    const handleFormChange = (event) => {
+        const value = event.target.value;
+        const inputName = event.target.name;
+
+        setFormData({
+            ...formData, [inputName]: value
+        });
+    }
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="offset-3 col-6">
+                    <div className="shadow p-4 mt-4">
+                        <h1>Add a Salesperson</h1>
+                        <form onSubmit={handleSubmit} id="add-salesperson-form">
+
+                            <div className="form-floating mb-3">
+                                <input onChange={handleFormChange} value={formData.first_name} placeholder="First Name" required type="text" name="first_name" id="first_name" className="form-control" />
+                                <label htmlFor="first_name">First Name</label>
+                            </div>
+
+                            <div className="form-floating mb-3">
+                                <input onChange={handleFormChange} value={formData.last_name} placeholder="Last Name" required type="text" name="last_name" id="last_name" className="form-control" />
+                                <label htmlFor="last_name">Last Name</label>
+                            </div>
+
+                            <button className="btn btn-primary">Add</button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AddSalesperson;
--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -4,16 +4,25 @@ class AutomobileVO(models.Model):
     vin = models.CharField(max_length=300, primary_key=True)
     sold = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.vin
+
 class Salesperson(models.Model):
     employee_id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     address = models.TextField()
     phone_number = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 class Sale(models.Model):
     price = models.PositiveIntegerField()
