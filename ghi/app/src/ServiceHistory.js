dbf85c1d93c3631d7c832fb3e1b7aa75a8d1ecb3
Elian Felix <efelix.v@gmail.com>
2024-02-07 09:31:52 -0500

    Change a naming convetion for status field

--- ghi/app/src/ServiceHistory.js
+++ ghi/app/src/ServiceHistory.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 
-const statuses = ['Pending', 'Finished', 'Canceled']
+const statuses = ['Created', 'Finished', 'Canceled']
 
 function ServiceHistory() {
     const [appmts, setAppmts] = useState([]);
