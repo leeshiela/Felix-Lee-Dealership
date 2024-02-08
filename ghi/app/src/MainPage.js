8ab21b559d51f552da32fc43cbda66b600693375
Shiela Lee <leeshiela@gmail.com>
2024-02-08 11:34:00 -0500

    Fixed homepage and logo

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,11 +1,10 @@
 import felix_lee_name from "./static/img/felix_lee_name.png";
+import { NavLink } from 'react-router-dom';
 
 function MainPage() {
   return (
     <div className="px-4 py-5 my-5 text-center">
-        <div id="dealership-div">
-          <img src={felix_lee_name} alt="dealership name"/>
-        </div>
+      <img id="dealership-name" src={felix_lee_name} alt="dealership name"/>
       <h2 className="display-sm fw-bold">Car Showroom</h2>
       <div className="col-lg-6 mx-auto">
         <p className="lead mb-4">
@@ -13,15 +12,19 @@ function MainPage() {
         </p>
       </div>
       <div className="container">
-        <div className="row gx-5">
-          <div className="card col-md h-100" >
-            <h3>I'm looking for a car</h3>
-            <img className="card-img-top" src="https://car-images.bauersecure.com/wp-images/3033/1056x594/020_tesla_model_3.jpg" alt="tesla customer"/>
+        <div className="row">
+          <div className="card col-md adjust" >
+            <NavLink to="/automobiles/">
+              <h3>I'm looking for a car</h3>
+              <img className="card-img-top" src="https://car-images.bauersecure.com/wp-images/3033/1056x594/020_tesla_model_3.jpg" alt="tesla customer"/>
+            </NavLink>
           </div>
-          <div className="card col-md h-100">
-            <h3>I need to service my car</h3>
-            <img className="card-img-top" src="https://scitexas.edu/wp-content/uploads/2021/05/auto-hero-2.jpg"/>
-          </div>
+          <div className="card col-md adjust">
+            <NavLink to="/appointments/create/">
+              <h3>I need to service my car</h3>
+              <img className="card-img-top" src="https://scitexas.edu/wp-content/uploads/2021/05/auto-hero-2.jpg"/>
+            </NavLink>
+            </div>
         </div>
       </div>
     </div>
--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -3,40 +3,41 @@ import felix_lee_black from "./static/img/felix_lee_black.png";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-black">
-      <div className="container-fluid flex-wrap">
-        <div id="logo">
-          <div>
-            <NavLink className="navbar-brand" to="/"><img src={felix_lee_black} alt="logo link"/></NavLink>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-black">
+        <div className="navbar-brand" >
+          <NavLink  to="/"><img id="logo" src={felix_lee_black} alt="logo link"/></NavLink>
+        </div>
+        <div className="collapse navbar-collapse flex-wrap" id="navbarNavDropdown">
+          <NavLink className="navbar-brand" to="/models/">Models</NavLink>
+          <NavLink className="navbar-brand" to="/models/create/">Create a Model</NavLink>
+          <NavLink className="navbar-brand" to="/manufacturers/">Manufacturers</NavLink>
+          <NavLink className="navbar-brand" to="/manufacturers/create/">Create a Manufacturer</NavLink>
+          <NavLink className="navbar-brand" to="/automobiles/">Automobiles</NavLink>
+          <NavLink className="navbar-brand" to="/automobiles/create/">Create an Automobile</NavLink>
+          <NavLink className="navbar-brand" to="/salespeople/">Salespeople</NavLink>
+          <NavLink className="navbar-brand" to="/salespeople/create/">Add a Salesperson</NavLink>
+          <NavLink className="navbar-brand" to="/customers/">Customers</NavLink>
+          <NavLink className="navbar-brand" to="/customers/create/">Add a Customer</NavLink>
+          <NavLink className="navbar-brand" to="/sales/">Sales</NavLink>
+          <NavLink className="navbar-brand" to="/sales/create/">Add a Sale</NavLink>
+          <NavLink className="navbar-brand" to="/sales/history/">Sales History</NavLink>
+          <NavLink className="navbar-brand" to="/technicians/">Technicians</NavLink>
+          <NavLink className="navbar-brand" to="/technicians/create/">Add a Technician</NavLink>
+          <NavLink className="navbar-brand" to="/appointments/">Service Appointments</NavLink>
+          <NavLink className="navbar-brand" to="/appointments/create/">Create a Service appointment</NavLink>
+          <NavLink className="navbar-brand" to="/appointments/history/">Service History</NavLink>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            </ul>
           </div>
         </div>
-        <NavLink className="navbar-brand" to="/manufacturers/">Manufacturers</NavLink>
-        <NavLink className="navbar-brand" to="/manufacturers/create/">Create a Manufacturer</NavLink>
-        <NavLink className="navbar-brand" to="/models/">Models</NavLink>
-        <NavLink className="navbar-brand" to="/models/create/">Create a Model</NavLink>
-        <NavLink className="navbar-brand" to="/automobiles/">Automobiles</NavLink>
-        <NavLink className="navbar-brand" to="/automobiles/create/">Create an Automobile</NavLink>
-        <NavLink className="navbar-brand" to="/salespeople/">Salespeople</NavLink>
-        <NavLink className="navbar-brand" to="/salespeople/create/">Add a Salesperson</NavLink>
-        <NavLink className="navbar-brand" to="/customers/">Customers</NavLink>
-        <NavLink className="navbar-brand" to="/customers/create/">Add a Customer</NavLink>
-        <NavLink className="navbar-brand" to="/sales/">Sales</NavLink>
-        <NavLink className="navbar-brand" to="/sales/create/">Add a Sale</NavLink>
-        <NavLink className="navbar-brand" to="/sales/history/">Sales History</NavLink>
-        <NavLink className="navbar-brand" to="/technicians/">Technicians</NavLink>
-        <NavLink className="navbar-brand" to="/technicians/create/">Add a Technician</NavLink>
-        <NavLink className="navbar-brand" to="/appointments/">Service Appointments</NavLink>
-        <NavLink className="navbar-brand" to="/appointments/create/">Create a Service appointment</NavLink>
-        <NavLink className="navbar-brand" to="/appointments/history/">Service History</NavLink>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          </ul>
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </>
+
   )
 }
 
